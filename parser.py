87d4257326aad3d49737f5a013e85f85561e220f
Wu-Chen Su <wu-chensu@Wu-Chens-MacBook-Pro.local>
2021-06-01 09:36:49 -0400

    update files

--- parser.py
+++ parser.py
@@ -1,44 +1,46 @@
 import pandas as pd
 import json
+import os
 
 
-rel_results={}
-rel_sources={}
-doc={}
+def load_data(data_folder):
 
-source_part=[]
-current=0
+    rel_results={}
+    rel_sources={}
+    doc={}
+    source_part=[]
+    current=0
 
-###Load data and assign column types to avoid warings, and reanme for certain columns to avoid confusions
-mrrel_df = pd.read_csv('MRREL.RRF',sep="|")
-mrrel_df.rename(columns={'SAB':'REL_SOURCE'},inplace=True)
+    mrrel_file = os.path.join(data_folder,"MRREL.RRF")
+    mrconso_file=os.path.join(data_folder,"MRCONSO.RRF")
 
-mrconso_df=pd.read_csv('MRCONSO.RRF',sep="|",dtype={'CUI': 'string', 'AUI': 'string', 'STR': 'string', 'SAB': 'string', 'SCODE': 'string', 'ISPREF': 'string'})
-mrconso_df.rename(columns={'SAB':'ATOM_SOURCE'},inplace=True)
+    ###Load data and assign column types to avoid warings, and reanme for certain columns to avoid confusions
+    mrrel_df = pd.read_csv(mrrel_file,sep="|")
+    mrrel_df.rename(columns={'SAB':'REL_SOURCE'},inplace=True)
 
-### Only include rows from UMLS and MEDDRA sources
-mrconso_rows=mrconso_df.loc[(mrconso_df['ATOM_SOURCE'] == 'UMLS') | (mrconso_df['ATOM_SOURCE'] == 'MEDDRA')]
+    mrconso_df=pd.read_csv(mrconso_file,sep="|",dtype={'CUI': 'string', 'AUI': 'string', 'STR': 'string', 'SAB': 'string', 'SCODE': 'string', 'ISPREF': 'string'})
+    mrconso_df.rename(columns={'SAB':'ATOM_SOURCE'},inplace=True)
 
-### Loop every CUI1 from the MRREL file
-ids=mrrel_df.query('CUI1 in @mrconso_rows.CUI').drop_duplicates(subset=['CUI1'])
+    ### Only include rows from UMLS and MEDDRA sources
+    mrconso_rows=mrconso_df.loc[(mrconso_df['ATOM_SOURCE'] == 'UMLS') | (mrconso_df['ATOM_SOURCE'] == 'MEDDRA')]
 
-with open('data.json', 'a') as outfile:
+    ### Loop every CUI1 from the MRREL file
+    ids=mrrel_df.query('CUI1 in @mrconso_rows.CUI').drop_duplicates(subset=['CUI1'])
+    for item in ids['CUI1']:
+      current+=1
+      print("current count:",current)
+      cui1=item.strip()
+      mrrel_rows=mrrel_df.loc[(mrrel_df['CUI1'] == cui1)]
+
+      ###get needed columns for later use
+      mrrel_rows_subset=mrrel_rows[['CUI1','REL','CUI2','RUI','REL_SOURCE']] 
+      mrconso_rows_subset=mrconso_rows[['CUI','ATOM_SOURCE','STR','SCODE','ISPREF']] 
  
- for item in ids['CUI1']:
-  current+=1
-  print("current:",current)
-  cui1=item.strip()
-  mrrel_rows=mrrel_df.loc[(mrrel_df['CUI1'] == cui1)]
-
-  ###get needed columns for later use
-  mrrel_rows_subset=mrrel_rows[['CUI1','REL','CUI2','RUI','REL_SOURCE']] 
-  mrconso_rows_subset=mrconso_rows[['CUI','ATOM_SOURCE','STR','SCODE','ISPREF']] 
+      result=pd.merge(mrconso_rows_subset, mrrel_rows_subset, left_on='CUI', right_on='CUI2')
+      result=result.drop_duplicates(subset=['CUI1','ATOM_SOURCE','REL','STR','SCODE','RUI','REL_SOURCE','ISPREF'])
  
-  result=pd.merge(mrconso_rows_subset, mrrel_rows_subset, left_on='CUI', right_on='CUI2')
-  result=result.drop_duplicates(subset=['CUI1','ATOM_SOURCE','REL','STR','SCODE','RUI','REL_SOURCE','ISPREF'])
- 
-  """ 
-  generate part of JSON example  
+      """ 
+      generate part of JSON example  
       "has_adverse_effect_on": [
             {
                 "name": "Renal and urinary disorders",
@@ -53,39 +55,41 @@ with open('data.json', 'a') as outfile:
                     }
                 ]
             },
-  """
-  for index, row in result.iterrows():
-   cui=row['CUI1']
-   atom_source=row['ATOM_SOURCE']
-   rel=row['REL']
-   t_str=row['STR']
-   t_scode=row['SCODE']
-   rui=row['RUI']
-   rel_source=row['REL_SOURCE']
-   is_pref=row['ISPREF']
+      """
+      for index, row in result.iterrows():
+        cui=row['CUI1']
+        atom_source=row['ATOM_SOURCE']
+        rel=row['REL']
+        t_str=row['STR']
+        t_scode=row['SCODE']
+        rui=row['RUI']
+        rel_source=row['REL_SOURCE']
+        is_pref=row['ISPREF']
    
-   rel_sources= {'name':'iDisk', 'record':rui}
-   rel_sources1= {'name':rel_source}
-   source_part=[]
-   source_part.append(rel_sources)
-   source_part.append(rel_sources1)
-   json_dict = {'name':t_str,atom_source.lower():atom_source+":"+t_scode,'source':source_part}
-   rel_results.setdefault(rel,[]).append(json_dict)
+        rel_sources= {'name':'iDisk', 'record':rui}
+        rel_sources1= {'name':rel_source}
+        source_part=[]
+        source_part.append(rel_sources)
+        source_part.append(rel_sources1)
+        json_dict = {'name':t_str,atom_source.lower():atom_source+":"+t_scode,'source':source_part}
+        rel_results.setdefault(rel,[]).append(json_dict)
 
   
-  ###Every atom from cui1 has the same copy in the json file
-  mrconso_rows_tmp=mrconso_rows.loc[(mrconso_rows['CUI'] == cui)].drop_duplicates(subset=['SCODE','STR','ATOM_SOURCE'])
-  for index, row in mrconso_rows_tmp.iterrows():
-       s_scode=row['SCODE']
-       s_str=row['STR']
-       sab=row['ATOM_SOURCE']
-       _id=sab+":"+s_scode
-       rec={"name": s_str ,sab.lower(): sab+":"+s_scode}
-       for rel,doc1 in rel_results.items():
+      ###Every atom from cui1 has the same copy in the json file
+      mrconso_rows_tmp=mrconso_rows.loc[(mrconso_rows['CUI'] == cui)].drop_duplicates(subset=['SCODE','STR','ATOM_SOURCE'])
+      for index, row in mrconso_rows_tmp.iterrows():
+        s_scode=row['SCODE']
+        s_str=row['STR']
+        sab=row['ATOM_SOURCE']
+        _id=sab+":"+s_scode
+        rec={"name": s_str ,sab.lower(): sab+":"+s_scode}
+        for rel,doc1 in rel_results.items():
            rec[rel]=doc1
-       doc.setdefault(_id,rec) 
-  rel_results={} 
-     
- json.dump(doc, outfile, indent=4) ###Dump json objects into file
+        doc.setdefault(_id,rec) 
+      rel_results={} 
+   
+    return doc  
+
+
  
 
